a2455c62c668de1a11b48468b9985dc2de07cc2a
anthonycsiracusa <anthonycsiracusa@gmail.com>
2016-02-07 17:05:06 -0600

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -1,138 +1,84 @@
-// code adapted from https://www.mapbox.com/mapbox.js/example/v1.0.0/markers-with-image-slideshow/
+<html>
+<head>
+  <script src='https://api.tiles.mapbox.com/mapbox.js/v2.2.4/mapbox.js'></script>
+<link href='https://api.tiles.mapbox.com/mapbox.js/v2.2.4/mapbox.css' rel='stylesheet' />
+<meta charset=utf-8 />
+<title>Lynchings in Shelby County</title>
 
-L.mapbox.accessToken =
-	'pk.eyJ1IjoidnVsaWJyYXJ5Z2lzIiwiYSI6ImFaN2JkSlUifQ.Dl2sTO1mGKK7MCd1ViGPnQ';
+<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+<script src='https://api.mapbox.com/mapbox.js/v2.3.0/mapbox.js'></script>
+<link href='https://api.mapbox.com/mapbox.js/v2.3.0/mapbox.css' rel='stylesheet' />
+<style>
+  body { margin:0; padding:0; }
+  #map { position:absolute; top:0; bottom:0; width:100%; }
+</style>
 
-// If you do not have a Mapbox key, refer to the readme.md
-var map = L.mapbox.map('map').setView([52.51, 13.38], 12);
 
-var layer_id = "vulibrarygis.of23e6p0";
-var layer = L.mapbox.tileLayer(layer_id);
-layer.addTo(map);
+</head>
+<body>
+<style>
+.leaflet-popup-content img {
+  max-width:100%;
+  }
+</style>
+<div id='map'></div>
 
-// Add custom popup html to each marker
-map.markerLayer.on('layeradd', function(e) {
-	var marker = e.layer;
-	var feature = marker.feature;
-	var images = feature.properties.images;
-	var slideshowContent = '';
+<script>
+L.mapbox.accessToken = 'pk.eyJ1IjoiYW50aG9ueWNzaXJhY3VzYSIsImEiOiJjaWdudWVocG4wMGJpbmtrb2ZkZDNpczF1In0.m3FSJwiHgwy1pfcsP2Orbw';
+	var map = L.mapbox.map('map', 'anthonycsiracusa.p31dhnah').setView([35.14, -89.98], 10);
 
-	for (var i = 0; i < images.length; i++) {
-		var img = images[i];
+var myLayer = L.mapbox.featureLayer().addTo(map);
 
-		slideshowContent += '<div class="image' + (i === 0 ? ' active' : '') + '">' +
-			img[0] +
-			'<div class="caption">' + img[1] + '</div>' +
-			'</div>';
-	}
+var geoJson = [{
+    type: 'Feature',
+    "geometry": { "type": "Point", "coordinates": [-89.85, 35.12]},
+    "properties": {
+        "image": "http://projects.library.vanderbilt.edu/civil/files/original/d710fa538b6d805749c2cec3068126e5.png",
+        "url": "http://projects.library.vanderbilt.edu/civil/items/show/3",
+        "marker-color": "#604020",
+        "marker-size": "medium",
+        "city": "Site of Ell Persons Lynching."
+    }
+}, {
+    type: 'Feature',
+    "geometry": { "type": "Point", "coordinates": [-90.05, 35.14]},
+    "properties": {
+        "image": "http://projects.library.vanderbilt.edu/civil/files/original/16ff294a4e095b4cb53c32098f3e5a0b.png",
+        "url": "http://projects.library.vanderbilt.edu/civil/items/show/3",
+        "marker-color": "#604020",
+        "city": "Beale Street - Where Ell Persons' head was thrown into the street."
+    }
+}, {
+    type: 'Feature',
+    "geometry": { "type": "Point", "coordinates": [-90.054807, 35.140303]},
+    "properties": {
+        "image": "http://projects.library.vanderbilt.edu/civil/files/original/0ff434cb0fa7d1ab5a4ae2cdd2845e1d.png",
+        "url": "http://projects.library.vanderbilt.edu/civil/items/show/3",
+        "marker-color": "#604020",
+        "city": "Original Location of Shelby County Jail"
+    }
+}];
 
-	// Create custom popup content
-	var popupContent = '<div id="' + feature.properties.id + '" class="popup">' +
-		'<h2>' + feature.properties.title + '</h2>' +
-		'<div class="slideshow">' +
-		slideshowContent +
-		'</div>' +
-		'<div class="cycle">' +
-		'<a href="#" class="prev">&laquo; Previous</a>' +
-		'<a href="#" class="next">Next &raquo;</a>' +
-		'</div>';
-	'</div>';
+// Add custom popups to each using our custom feature properties
+myLayer.on('layeradd', function(e) {
+    var marker = e.layer,
+        feature = marker.feature;
 
-	// http://leafletjs.com/reference.html#popup
-	marker.bindPopup(popupContent, {
-		closeButton: false,
-		maxWidth: 200,
-		autoPan: true,
-		keepInView: true
-	});
+    // Create custom popup content
+    var popupContent =  '<a target="_blank" class="popup" href="' + feature.properties.url + '">' +
+                            '<img src="' + feature.properties.image + '" />' +
+                            feature.properties.city +
+                        '</a>';
+
+    // http://leafletjs.com/reference.html#popup
+    marker.bindPopup(popupContent,{
+        closeButton: false,
+        minWidth: 320
+    });
 });
 
-// This example uses jQuery to make selecting items in the slideshow easier.
-// Download it from http://jquery.com
-$('#map').on('click', '.popup .cycle a', function() {
-	var $slideshow = $('.slideshow'),
-		$newSlide;
-
-	if ($(this).hasClass('prev')) {
-		$newSlide = $slideshow.find('.active').prev();
-		if ($newSlide.index() < 0) {
-			$newSlide = $('.image').last();
-		}
-	} else {
-		$newSlide = $slideshow.find('.active').next();
-		if ($newSlide.index() < 0) {
-			$newSlide = $('.image').first();
-		}
-	}
-
-	$slideshow.find('.active').removeClass('active').hide();
-	$newSlide.addClass('active').show();
-	return false;
-});
-
-// Get the points from Cloudant using JSONP
-// http://stackoverflow.com/questions/14220321/how-to-return-the-response-from-an-ajax-call
-$(function() {
-
-	// list views from Cloudant that we want to offer as layers
-	var cloudantViews = [];
-	$.getJSON('https://vulibrarygis.cloudant.com/mapping-berlin/_design/tour/',
-		function(result) {
-			var viewsList = result.views;
-			for (var v in viewsList) {
-				cloudantViews.push(v);
-			}
-
-			// put each view into the dropdown menu
-			$.each(cloudantViews, function(i, viewname) {
-				$('#layers-dropdown').append('<option value="' + viewname + '">' +
-					viewname + '</option>');
-			});
-		});
-
-	// when the user selects from the dropdown, change the layer
-	$('#layers-dropdown').change(function() {
-		var selection_label = $('#layers-dropdown option:selected').text();
-		var selection_value = $('#layers-dropdown').val();
-		if (selection_value !== 'default') {
-			var thisCloudantView = selection_value;
-			getLayer(processLayer, thisCloudantView);
-		}
-	});
-});
-
-function getLayer(callback, cloudantView) {
-	var cloudantURLbase =
-		"https://vulibrarygis.cloudant.com/mapping-berlin/_design/tour/_view/";
-	var cloudantURLcallback = "?callback=?";
-	var thisCloudantURL = cloudantURLbase + cloudantView + cloudantURLcallback;
-	$.getJSON(thisCloudantURL, function(result) {
-		var points = result.rows;
-		var geoJSON = [];
-		for (var i in points) {
-			geoJSON["locations"] = geoJSON.push(points[i].value);
-		}
-		callback(geoJSON);
-	});
-}
-
-function processLayer(result) {
-	// Add features to the map
-	var selection_label = $('#layers-dropdown option:selected').text();
-	if (selection_label == "1908") {
-		new_id = 'vulibrarygis.l74iic1a'
-	} else if (selection_label == "1920") {
-		new_id = 'vulibrarygis.l366jopj'
-	} else if (selection_label == "1936") {
-		new_id = 'vulibrarygis.l369lc2l'
-	} else if (selection_label == "1947") {
-		new_id = 'vulibrarygis.l36anlai'
-	} else if (selection_label == "1970") {
-		new_id = 'vulibrarygis.l36db1a5'
-	} else {
-		new_id = 'vulibrarygis.of23e6p0'
-	};
-	var new_layer = L.mapbox.tileLayer(new_id);
-	new_layer.addTo(map);
-	map.featureLayer.setGeoJSON(result);
-}
+// Add features to the map
+myLayer.setGeoJSON(geoJson);
+</script>
+</body>
+</html>
