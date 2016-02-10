4b933768c1417f8fe287fa86bef4c3481e916f76
anthonycsiracusa <anthonycsiracusa@gmail.com>
2016-02-10 13:17:07 -0600

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -130,7 +130,7 @@ function processLayer(result) {
 	} else if (selection_label == "1970") {
 		new_id = 'vulibrarygis.l36db1a5'
 	} else {
-		new_id = 'vulibrarygis.of23e6p0'
+		new_id = 'anthonycsiracusa.p31dhnah'
 	};
 	var new_layer = L.mapbox.tileLayer(new_id);
 	new_layer.addTo(map);
